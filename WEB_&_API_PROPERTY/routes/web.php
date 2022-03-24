325e538c9a92c658efce8fb1304d3ba8b920d1e7
zhafyrah <zhafirahn46@gmail.com>
2022-03-24 20:24:36 +0700

    zhafy

--- WEB_&_API_PROPERTY/routes/web.php
+++ WEB_&_API_PROPERTY/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PelatihController;
+use App\Http\Controllers\RumahController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,26 +23,34 @@ Route::get('/', function () {
 //     return view('/layouts/template');
 // });
 
-Auth::routes();
+// Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 // Route::group(["middleware" => ["admin"]], function() {
 
-    Route::prefix("admin")->group(function() {
+Route::prefix("admin")->group(function () {
 
-        Route::get("/dashboard", [AdminController::class, "dashboard"]);
+    Route::get("/dashboard", [AdminController::class, "dashboard"]);
 
-        Route::prefix("pelatih")->group(function() {
-            Route::get("/", [PelatihController::class, "index"]);
-            Route::post("/store", [PelatihController::class, "store"]);
-            Route::get("/tambah_data", [PelatihController::class, "tambah_data"]);
-            Route::get("/edit/{id}", [PelatihController::class, "edit"]);
-            Route::get("/detail/{id}", [PelatihController::class, "detail"]);
-            Route::post("/hapus/", [PelatihController::class, "destroy"]);
-            Route::post("/update", [PelatihController::class, "update"]);
-        });
-
+    Route::prefix("pelatih")->group(function () {
+        Route::get("/", [PelatihController::class, "index"]);
+        Route::post("/store", [PelatihController::class, "store"]);
+        Route::get("/tambah_data", [PelatihController::class, "tambah_data"]);
+        Route::get("/edit/{id}", [PelatihController::class, "edit"]);
+        Route::get("/detail/{id}", [PelatihController::class, "detail"]);
+        Route::post("/hapus", [PelatihController::class, "destroy"]);
+        Route::post("/update", [PelatihController::class, "update"]);
     });
+
+    Route::prefix("rumah")->group(function () {
+        Route::get("/", [RumahController::class, "index"]);
+        Route::post("/store", [RumahController::class, "store"]);
+        Route::get("/tambah_rumah" . [RumahController::class, "tambah_data"]);
+        Route::get("/edit/{id}", [RumahController::class, "edit_rumah"]);
+        Route::post("/hapus", [RumahController::class, "destroy"]);
+        Route::post("/update", [RumahController::class, "update"]);
+    });
+});
 
 // });
