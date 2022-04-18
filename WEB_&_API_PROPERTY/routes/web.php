df926af1bde1fb2432e702f56855cdbbdb2d6300
Zhafyrah <zhafirahn46@gmail.com>
2022-04-18 16:36:04 +0700

    update

--- WEB_&_API_PROPERTY/routes/web.php
+++ WEB_&_API_PROPERTY/routes/web.php
@@ -26,41 +26,37 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
+
 Route::group(["middleware" => ["guest"]], function() {
-=======
+
 // Route::get('/admin', function () {
 //     return view('/layouts/template');
 // });
 
-Auth::routes();
+// Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index']);
 
 // Route::group(["middleware" => ["admin"]], function() {
-<<<<<<< HEAD
+
 Route::get("/login", [TampilanLoginController::class, "TampilanLogin"]);
 Route::get("/register", [TampilanLoginController::class, "TampilanRegistrasi"]);
-=======
->>>>>>> 84399849b777d3416fb545bbbc9614f94a3aa252
+
+
     Route::get("/login", [TampilanLoginController::class, "TampilanLogin"]);
     Route::get("/register", [TampilanLoginController::class, "TampilanRegistrasi"]);
     Route::post("/login", [LoginController::class, "post_login"]);
 });
->>>>>>> 65a5966a9465743ad8da85dcba4543130db1c013
-
-<<<<<<< HEAD
 
 Route::group(["middleware" => ["admin"]], function() {
-=======
+
 Route::prefix("admin")->group(function () {
 
     Route::get("/dashboard", [AdminController::class, "dashboard"]);
 
-<<<<<<< HEAD
+
     Route::prefix("users")->group(function () {
-=======
-<<<<<<< HEAD
+
     Route::prefix("pelatih")->group(function () {
         Route::get("/", [PelatihController::class, "index"]);
         Route::post("/store", [PelatihController::class, "store"]);
@@ -71,7 +67,7 @@ Route::prefix("admin")->group(function () {
         Route::post("/update", [PelatihController::class, "update"]);
     });
 });
-=======
+
         Route::prefix("users")->group(function() {
             Route::get("/", [AkunController::class, "index"]);
             Route::post("/tambah/", [AkunController::class, "tambah"]);
@@ -79,13 +75,13 @@ Route::prefix("admin")->group(function () {
             Route::get("/edit/{id_role}", [AkunController::class, "edit"]);
             Route::post("/simpan", [AkunController::class, "simpan"]);
         });
->>>>>>> 84399849b777d3416fb545bbbc9614f94a3aa252
 
-    
+
+
 Route::prefix("admin")->group(function() {
-    
+
     Route::get("/dashboard", [AdminController::class, "dashboard"]);
-    
+
     Route::prefix("pelatih")->group(function() {
         Route::get("/", [PelatihController::class, "index"]);
         Route::post("/store", [PelatihController::class, "store"]);
@@ -95,9 +91,9 @@ Route::prefix("admin")->group(function() {
         Route::post("/hapus/", [PelatihController::class, "destroy"]);
         Route::post("/update", [PelatihController::class, "update"]);
     });
-    
+
     Route::prefix("users")->group(function() {
->>>>>>> 65a5966a9465743ad8da85dcba4543130db1c013
+
         Route::get("/", [AkunController::class, "index"]);
         Route::post("/tambah/", [AkunController::class, "tambah"]);
         Route::post("/hapus", [AkunController::class, "hapus"]);
@@ -124,7 +120,7 @@ Route::prefix("rumah")->group(function () {
 });
 
     Route::get("/logout", [LoginController::class, "logout"]);
-    
+
 });
 
 // Route::prefix("users")->group(function() {
@@ -134,18 +130,18 @@ Route::prefix("rumah")->group(function () {
     //     Route::get("/edit/{id_role}", [AkunController::class, "edit"]);
     //     Route::post("/simpan", [AkunController::class, "simpan"]);
     // });
-    
+
     Route::group(["middleware" => ["guest"]], function() {
-        
+
         Route::prefix("admin")->group(function() {
-            
+
             Route::get("/login", [LoginController::class, "login"]);
-            
+
             Route::post("/post_login", [LoginController::class, "post_login"] );
-            
+
         });
-        
-    });    
+
+    });
 });
 
 Route::prefix("owner")->group(function() {
